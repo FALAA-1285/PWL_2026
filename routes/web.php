530cdf43374f93dd341c460ce7d22d095c58983d
Ahmad Falahi <ahmadfalahi1285@gmail.com>
2026-02-26 08:07:25 -0400

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('Selamat Datang');
 });
+
+Route::get('/about', function (){
+    return 'Nama : Ahmad Falahi<br> NIM : 244107020152';
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentID){
+    return 'Pos ke-'.$postID.' Komentar ke-: '.$commentID;
+});
+    
+Route::get('/articles/{id}', function ($id){
+    return 'Halaman article dengan ID '.$id;
+});
+                
+Route::get('/hello', function (){
+    return 'Hello World';
+});
+            
+Route::get('/world', function(){
+    return 'world';
+});
+                
+                
