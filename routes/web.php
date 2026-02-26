7e132dcf6e8e60f743f0ddf6745ac392b2bb3cb3
Ahmad Falahi <ahmadfalahi1285@gmail.com>
2026-02-26 10:57:04 -0400

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return ('Selamat Datang');
-});
 
-Route::get('/about', function (){
-    return 'Nama : Ahmad Falahi<br> NIM : 244107020152';
-});
 
-Route::get('/user/{name?}', function ($name='John'){
-    return 'Nama Saya '.$name;
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama Saya ' . $name;
 });
 
-Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentID){
-    return 'Pos ke-'.$postID.' Komentar ke-: '.$commentID;
+Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentID) {
+    return 'Pos ke-' . $postID . ' Komentar ke-: ' . $commentID;
 });
-    
-Route::get('/articles/{id}', function ($id){
-    return 'Halaman article dengan ID '.$id;
-});
-                
-Route::get('/hello', function (){
-    return 'Hello World';
-});
-            
-Route::get('/world', function(){
+
+Route::get('/world', function () {
     return 'world';
 });
-                
-                
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+use App\Http\Controllers\HomeController;
+Route::get('/', [HomeController::class, 'home']);
+
+use App\Http\Controllers\AboutController;
+Route::get('/about', [AboutController::class, 'about']);
+
+use App\Http\Controllers\ArticleController;
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([ 
+'index', 'show' 
+]); 
+Route::resource('photos', PhotoController::class)->except([ 
+'create', 'store', 'update', 'destroy' 
+]); 
